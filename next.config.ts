86feb994a1da2b72b36c3ab303b20bdbac779e3c
valter alfonso <valter7305@gmail.com>
2026-08-25 12:02:27 -0400

    chore: remove base path configuration from Next.js settings

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
-const basePath =
-  process.env.NODE_ENV === 'production' ? '/unchained-landing-page' : '';
-
 const nextConfig: NextConfig = {
-  basePath,
   output: 'export',
   images: {
     unoptimized: true,
